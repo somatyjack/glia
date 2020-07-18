33eb3bed9709815932afe7388849151be4070295
jack_milles <jack_milles@mail.ru>
2020-07-18 14:36:57 +0200

    new utils for axios

--- middleware/mapService.js
+++ middleware/mapService.js
@@ -2,7 +2,7 @@ const mapService = (req, res, next) => {
     // get route based on first 2 values
     const subPath = req.path.split("/");
     const fullPath = `/${subPath[1]}/${subPath[2]}`;
-    req.serviceName = req.app.kernel.routeMap[fullPath];
+    req.serviceName = req.app.kernel.routeMap[req.method][fullPath];
 
     next();
 };
---
+++ utils/utils.js
@@ -0,0 +1,102 @@
+const logger = require("glia/middleware/logger/logger");
+const axios = require("axios");
+
+// abstraction functions to avoid writing try/catch in each service
+const utils = {
+    //wrapping function, as it may be changed to queues in the future
+    GetData: async (url, params = {}) => {
+        let newParams = params;
+
+        try {
+            const { data } = await axios.get(url, { params: newParams });
+            if (!data.success) return false;
+            else return data.response;
+        } catch (e) {
+            logger.log("error", e.message, `GetData - ${url}`);
+        }
+    },
+    //wrapping function, as it may be changed to queues in the future
+    PostData: async (url, params = {}) => {
+        let newParams = params;
+
+        try {
+            const { data } = await axios({
+                method: "post",
+                url: url,
+                data: newParams,
+                config: { headers: { "Content-Type": "multipart/form-data" } },
+            });
+            if (!data.success) return false;
+            else return data.response;
+        } catch (e) {
+            logger.log("error", e.message, `PostData - ${url}`);
+        }
+
+        /*
+                .then(function (res) {
+                    resolve(res.data);
+                })
+                .catch((error) => {
+                    if (error.code == "ECONNREFUSED")
+                        console.log("Refused to connect to:" + url);
+
+                    reject(error.response.data);
+                });
+                */
+    },
+    DeleteData: async (url, params = {}) => {
+        let newParams = params;
+
+        try {
+            const { data } = await axios.delete(url, {
+                data: newParams,
+            });
+            if (!data.success) return false;
+            else return data.response;
+        } catch (e) {
+            logger.log("error", e.message, `PostData - ${url}`);
+        }
+        /*
+                .then(function (res) {
+                    resolve(res.data);
+                })
+                .catch((error) => {
+                    if (error.code == "ECONNREFUSED")
+                        console.log("Refused to connect to:" + url);
+
+                    reject(error.response.data);
+                });
+                */
+    },
+    ConvertToHoursMins: (time) => {
+        if (time < 1) return "00:00";
+
+        let hours = Math.floor(time / 60);
+        hours = hours < 10 ? "0" + hours : hours;
+
+        let minutes = time % 60;
+        minutes = minutes > 10 ? minutes : minutes + "0";
+
+        return hours + ":" + minutes;
+    },
+    // wrapping function, as it may be changed to queues in the future
+    // function used for user logging and activities
+    PostAndForget: function (url, params = {}) {
+        let newParams = params;
+        newParams["requestType"] = "internal";
+
+        axios({
+            method: "post",
+            url: url,
+            data: newParams,
+            config: { headers: { "Content-Type": "multipart/form-data" } },
+        }).catch((error) => {
+            if (error.code == "ECONNREFUSED")
+                console.log("Refused to connect to:" + url);
+
+            console.log(error);
+        });
+    },
+};
+
+module.exports = utils;
